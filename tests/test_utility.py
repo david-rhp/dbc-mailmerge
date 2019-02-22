088cdcfebdb5830025a21446b683ba4d8a726d97
david-rhp <33631446+David-RHP@users.noreply.github.com>
2019-02-22 11:26:04 +0100

    Updated tests for creating the file hierarchy. Closes #2

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -3,9 +3,9 @@ from mailmerge.utility import path_creator, create_folder_hierarchy
 
 
 def test_path_creator():
-    test_input = [["advisor_1", "advisor_2"], ["ZU", "AP"]]
-    expected = [Path().joinpath("advisor_1", "ZU"), Path().joinpath("advisor_1", "AP"),
-                Path().joinpath("advisor_2", "ZU"), Path().joinpath("advisor_2", "AP")]
+    test_input = [["advisor_1", "advisor_2"], ["offer_documents", "appropriateness_test"]]
+    expected = [Path().joinpath("advisor_1", "offer_documents"), Path().joinpath("advisor_1", "appropriateness_test"),
+                Path().joinpath("advisor_2", "offer_documents"), Path().joinpath("advisor_2", "appropriateness_test")]
 
     paths = path_creator(test_input)
     assert paths == expected
@@ -15,10 +15,13 @@ def test_create_folder_hierarchy(tmp_path):
     top_level = "client_correspondence"
 
     base_path = tmp_path / top_level
-    expected_paths = [base_path.joinpath("advisor_1", "ZU"), base_path.joinpath("advisor_1", "AP"),
-                      base_path.joinpath("advisor_2", "ZU"), base_path.joinpath("advisor_2", "AP")]
+    expected_paths = [base_path.joinpath("advisor_1", "offer_documents"),
+                      base_path.joinpath("advisor_1", "appropriateness_test"),
+                      base_path.joinpath("advisor_2", "offer_documents"),
+                      base_path.joinpath("advisor_2", "appropriateness_test")]
 
-    create_folder_hierarchy(tmp_path, top_level, [["advisor_1", "advisor_2"], ["ZU", "AP"]])
+    create_folder_hierarchy(tmp_path, top_level, [["advisor_1", "advisor_2"],
+                                                  ["offer_documents", "appropriateness_test"]])
 
     # check if paths have been created by create_folder_hierarchy
     for path in expected_paths:
